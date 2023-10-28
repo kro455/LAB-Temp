fb9fb0b49e4c9316b18e2bb8b9509d3f5925f64a
kro455 <linhmoi455qt@gmail.com>
2023-10-28 22:55:58 +0700

    update

--- J1.S.P0070/src/EBank/Login/Ebank.java
+++ J1.S.P0070/src/EBank/Login/Ebank.java
@@ -16,6 +16,7 @@ public class Ebank {
         Locale.setDefault(locale);
     }
 
+    @SuppressWarnings("empty-statement")
     public void login() {
         while (print(checkAccountNumber(getInputString("enterAccountNumber"))));
         while (print(checkPassword(getInputString("enterPassword"))));
